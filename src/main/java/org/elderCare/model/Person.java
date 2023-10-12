fd561d1811984d3b196fa0d5810dbc9917a236e3
xiomarag55 <xiomisq1@gmail.com>
2023-10-11 21:55:46 -0500

    The model class person is created

---
+++ src/main/java/org/elderCare/model/Person.java
@@ -0,0 +1,80 @@
+package org.elderCare.model;
+
+public class Person {
+    private String name;
+    private int age;
+    private String identityDocument;
+    private String maritalStatus;
+    private int stratum;
+    private char isPension;
+    private char sex;
+
+    public char getSex() {
+        return sex;
+    }
+
+    public void setSex(char sex) {
+        this.sex = sex;
+    }
+
+    //Class constructor
+    public Person(String name, int age, String identityDocument, String maritalStatus, int stratum, char isPension, char sex) {
+        this.name = name;
+        this.age = age;
+        this.identityDocument = identityDocument;
+        this.maritalStatus = maritalStatus;
+        this.stratum = stratum;
+        this.isPension = isPension;
+        this.sex = sex;
+    }
+    //getters and setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getIdentityDocument() {
+        return identityDocument;
+    }
+
+    public void setIdentityDocument(String identityDocument) {
+        this.identityDocument = identityDocument;
+    }
+
+    public String getMaritalStatus() {
+        return maritalStatus;
+    }
+
+    public void setMaritalStatus(String maritalStatus) {
+        this.maritalStatus = maritalStatus;
+    }
+
+    public int getStratum() {
+        return stratum;
+    }
+
+    public void setStratum(int stratum) {
+        this.stratum = stratum;
+    }
+
+    public char getIsPension() {
+        return isPension;
+    }
+
+    public void setIsPension(char isPension) {
+        this.isPension = isPension;
+    }
+
+
+}
