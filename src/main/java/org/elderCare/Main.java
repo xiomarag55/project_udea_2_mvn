4c9f8a160f1187b620c17eb5ce9efd8d0636e659
xiomarag55 <xiomaragiraldo221@gmail.com>
2023-10-17 19:11:25 -0500

    Se agrega el resto de los métodos para trabajar en el proyecto

--- src/main/java/org/elderCare/Main.java
+++ src/main/java/org/elderCare/Main.java
@@ -4,12 +4,9 @@ import org.elderCare.repository.PersonInMemoryRepositoryImpl;
 import org.elderCare.repository.PersonRepository;
 import org.elderCare.service.PersonService;
 import org.elderCare.service.PersonServiceImpl;
-
-import java.util.logging.Logger;
 public class Main {
     public static void main(String[] args) {
 
-        Logger logger;
         PersonService personService = new PersonServiceImpl(new PersonInMemoryRepositoryImpl());
         PersonRepository personRepository = new PersonInMemoryRepositoryImpl();
 
--- src/main/java/org/elderCare/service/PersonServiceImpl.java
+++ src/main/java/org/elderCare/service/PersonServiceImpl.java
@@ -2,17 +2,20 @@ package org.elderCare.service;
 
 import org.elderCare.model.Person;
 import org.elderCare.repository.PersonInMemoryRepositoryImpl;
-import org.elderCare.repository.PersonRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class PersonServiceImpl implements PersonService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(PersonServiceImpl.class);
     public PersonServiceImpl(PersonInMemoryRepositoryImpl personInMemoryRepository){
 
     }
-
+    //Number of people who are in stratum 1 by id Number
     @Override
     public void peopleSituationVulnerability(List<Person> persons) {
         Map<String, Integer> vulnerablePerson = new HashMap<>();
@@ -25,9 +28,41 @@ public class PersonServiceImpl implements PersonService {
         }
         for (Map.Entry<String, Integer> person : vulnerablePerson.entrySet()) {
             System.out.println("Document: " + person.getKey() + " Stratum: " + person.getValue());
-
         }
+    }
 
+    @Override
+    public int totalSituationVulnerability(List<Person> persons) {
+        return 0;
+    }
 
+    @Override
+    public int averagePensioners(List<Person> persons) {
+        return 0;
+    }
+
+    @Override
+    public int numberPensioners(List<Person> persons) {
+        return 0;
+    }
+
+    @Override
+    public int numberNonePensioners(List<Person> persons) {
+        return 0;
+    }
+
+    @Override
+    public int numberPensionersOveSixty(List<Person> persons) {
+        return 0;
+    }
+
+    @Override
+    public int numberWomenPensioners(List<Person> persons) {
+        return 0;
+    }
+
+    @Override
+    public int numberWomenNonPensioners(List<Person> persons) {
+        return 0;
     }
 }
