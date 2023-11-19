318fda5477d0f033f41ff122d7481a7c6188204e
xiomarag55 <xiomisq1@gmail.com>
2023-11-19 16:54:23 -0500

    Other methods are added to bring the information from the database.

--- src/site/scripts/application.js
+++ src/site/scripts/application.js
@@ -19,10 +19,15 @@ function obtainPersons() {
                     <td>${person.sex}</td>
                 `;
                 result.appendChild(fila);
+                const listarBtn = document.getElementById('listarBtn');
+                if(listarBtn ){
+                    listarBtn .style.display = 'none';
+                }
             });
         })
         .catch(error => console.error('Error:', error));
 }
+//1
 function totalPersonsInSituationVulnerability() {
     fetch('http://localhost:8080/eldercare/persons/vulnerability', {
         method: 'GET'
@@ -31,6 +36,90 @@ function totalPersonsInSituationVulnerability() {
         .then(data => {
             const valorElement = document.getElementById('vulerabilityPerson');
             valorElement.textContent = data;
+        })
+        .catch(error => console.error('Error:', error));
+}
+//2
+function numberPensioners() {
+    fetch('http://localhost:8080/eldercare/persons/personPensioners', {
+        method: 'GET'
+    })
+        .then(response => response.json())
+        .then(data => {
+            const valorElement = document.getElementById('pensioners');
+            valorElement.textContent = data;
+        })
+        .catch(error => console.error('Error:', error));
+}
+//3
+function numberNonePensioners() {
+    fetch('http://localhost:8080/eldercare/persons/nonePensioners', {
+        method: 'GET'
+    })
+        .then(response => response.json())
+        .then(data => {
+            const valorElement = document.getElementById('nonPensioners');
+            valorElement.textContent = data;
+        })
+        .catch(error => console.error('Error:', error));
+}
+//4
+function numberPensionersOveSixty() {
+    fetch('http://localhost:8080/eldercare/persons/womenPensioners', {
+        method: 'GET'
+    })
+        .then(response => response.json())
+        .then(data => {
+            const valorElement = document.getElementById('overSixty');
+            valorElement.textContent = data;
+        })
+        .catch(error => console.error('Error:', error));
+}
+//5
+function numberWomenPensioners() {
+    fetch('http://localhost:8080/eldercare/persons/womenNonePensioners', {
+        method: 'GET'
+    })
+        .then(response => response.json())
+        .then(data => {
+            const valorElement = document.getElementById('womenPensioners');
+            valorElement.textContent = data;
+        })
+        .catch(error => console.error('Error:', error));
+}
+//6
+function numberWomenNonPensioners() {
+    fetch('http://localhost:8080/eldercare/persons/womenNonePensioners', {
+        method: 'GET'
+    })
+        .then(response => response.json())
+        .then(data => {
+            const valorElement = document.getElementById('womenNonPensioners');
+            valorElement.textContent = data;
+        })
+        .catch(error => console.error('Error:', error));
+}
+//7
+function percentPensioners() {
+    fetch('http://localhost:8080/eldercare/persons/percentPensioners', {
+        method: 'GET'
+    })
+        .then(response => response.json())
+        .then(data => {
+            const valorElement = document.getElementById('percentPensioners');
+            valorElement.textContent = `${data}%`;
+        })
+        .catch(error => console.error('Error:', error));
+}
+//8
+function percentNonPensioners() {
+    fetch('http://localhost:8080/eldercare/persons/percentNonePensioners', {
+        method: 'GET'
+    })
+        .then(response => response.json())
+        .then(data => {
+            const valorElement = document.getElementById('percentNonPensioners');
+            valorElement.textContent = `${data}%`;
         })
         .catch(error => console.error('Error:', error));
 }
